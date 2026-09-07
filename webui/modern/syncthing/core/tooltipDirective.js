8b0231bf758d41a6abe492cc23c981164438a743
Ilya Zarubin <36898027+ilyaZar@users.noreply.github.com>
2026-09-07 04:43:07 +0200

    fix: restrict state detail tooltips to value icons

--- webui/modern/syncthing/core/tooltipDirective.js
+++ webui/modern/syncthing/core/tooltipDirective.js
@@ -4,10 +4,15 @@ angular.module('syncthing.core')
             restrict: 'A',
             link: function (scope, element, attributes) {
                 var content = element.children('[tooltip-content]');
-                $(element).tooltip(content.length ? {
+                var options = content.length ? {
                     html: true,
                     title: function () { return content.html(); }
-                } : {});
+                } : {};
+                if (attributes.tooltipSelector) {
+                    options.selector = attributes.tooltipSelector;
+                    options.title = function () { return element.attr('data-original-title'); };
+                }
+                $(element).tooltip(options);
             }
         };
     });
